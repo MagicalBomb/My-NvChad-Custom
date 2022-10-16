fab9b57b7455d42b53f8eab24a7465a757674a5f
冒险岛真好玩 <17826800084g@gmail.com>
2022-10-16 15:21:12 +0800

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+# 依赖
+
+Mason 需要安装以下 LSP:
+- python-lsp-server
+- gopls: 提供了重命名， 语法高亮， lint 功能方面有问题需要依赖 golangci_lint_ls
+- golangci_lint_ls: 提供 lint 功能
+- marksman: markdown 语法 LSP
+
+
+
--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "gopls", "pylsp" }
+local servers = { "gopls", "pylsp", "golangci_lint_ls", "marksman" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
